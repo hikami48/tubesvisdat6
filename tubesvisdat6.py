4153dfbeabe6726229d289a9c3113e451ae8f218
adeirpan <adeirpan706@gmail.com>
2022-06-24 01:11:06 +0700

    aa

--- tubesvisdat6.py
+++ tubesvisdat6.py
@@ -1,65 +1,29 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
 import pandas as pd
-import numpy as np
-
 from bokeh.plotting import figure
-from bokeh.io import show, output_notebook, push_notebook, curdoc
-
-from bokeh.models import Quad, ColumnDataSource, HoverTool, CategoricalColorMapper, Panel, CustomJS, Dropdown, Select
+from bokeh.plotting import show
+from bokeh.io import curdoc
+from bokeh.models import ColumnDataSource, HoverTool, Select
+from bokeh.models.widgets import Tabs, Panel
+from bokeh.layouts import row, widgetbox
+from bokeh.palettes import Category20_16
 from bokeh.models.widgets import CheckboxGroup, Slider, RangeSlider, Tabs
-
 from bokeh.layouts import column, row, WidgetBox
-from bokeh.palettes import Category20_16, Spectral4
 
-from bokeh.application.handlers import FunctionHandler
-from bokeh.application import Application
-
-
-# In[12]:
+df = pd.read_csv("./data/covid_19_indonesia_time_series_all.csv")
+df["Date"] = pd.to_datetime(df["Date"])
+data = df[df["Location"].str.contains("Indonesia")==False]
+data = data[['Date', 'Location', 'Total Cases', 'Total Deaths', 'Total Recovered', 'Total Active Cases']]
 
 
-from bokeh.io import output_notebook, show
-
-from bokeh.plotting import figure 
-
-from bokeh.models import CustomJS, ColumnDataSource, Panel, Tabs, DatetimeTickFormatter, HoverTool, NumeralTickFormatter
-from bokeh.models.widgets import Select, DateRangeSlider
-
-from bokeh.application import Application
-from bokeh.application.handlers import FunctionHandler
-
-from bokeh.layouts import column, row
 
 
-# In[13]:
 
 
-data = pd.read_csv("Indonesia_coronavirus_daily_data.csv")
+lokasi = list(data.Location.unique())
 
+col_list = list(data.columns)
 
-# In[14]:
-
-
-data
-
-
-# In[15]:
-
-
-prov = list(data.Province.unique())
-
-DataList = list(data.columns)
-
-
-# In[16]:
-
-
-def newData(dt, feature):
+def make_dataset(lokasi, feature):
 
     
     xs = []
@@ -67,201 +31,250 @@ def newData(dt, feature):
     colors = []
     labels = []
 
-    for i, prov in enumerate(dt):
+    for i, lokasi in enumerate(lokasi):
 
-        dat = data[data['Province'] == prov].reset_index(drop = True)
+        df = data[data['Location'] == lokasi].reset_index(drop = True)
         
-        x = list(dat['Daily_Case'])
-        y = list(dat[feature])
+        x = list(df['Date'])
+        y = list(df[feature])
         
         xs.append(list(x))
         ys.append(list(y))
 
         colors.append(Category20_16[i])
-        labels.append(prov)
+        labels.append(lokasi)
 
     new_src = ColumnDataSource(data={'x': xs, 'y': ys, 'color': colors, 'label': labels})
 
     return new_src
 
+def make_plot(src, feature):
+    p = figure(plot_width = 700, plot_height = 400, 
+            title = 'Covid19-Indonesia All Time Series',
+            x_axis_label = 'date', y_axis_label = 'Feature Selected')
 
-# In[17]:
-
-
-#Functiuon to make the plot from the data obtained/updated at each selection in the ColumnDataSource
-def plotGraf(src, feature):
+    p.multi_line('x', 'y', color = 'color', legend_field = 'label', line_width = 2, source = src)
     
-    plot = figure(plot_width = 700, plot_height = 400, 
-            title = 'Death by Cumulative Case',
-            x_axis_label = 'Kasus Kumulative', y_axis_label = 'Jumlah Kematian')
-
-    plot.multi_line('x', 'y', color = 'color', legend_field = 'label', line_width = 2, source = src)
-
-    return plot
-
-
-# In[18]:
-
-
-def provinceUpdate(prv, dtLama, dtBaru):
+    tooltips = [
+            ('Date','$x'),
+            ('Total', '$y'),
+           ]
     
-    plot = [chbx.labels[i] for i in chbx.active]   
-    new_src = newData(plot, fitur.value)
+    p.add_tools(HoverTool(tooltips=tooltips))
+
+    return p
+
+def update_country(attr, old, new):
+    lokasi_plot = [lokasi_selection.labels[i] for i in lokasi_selection.active]
+
+    
+    new_src = make_dataset(lokasi_plot, feature_select.value)
+
     src.data.update(new_src.data)
 
-
-# In[19]:
-
-
-def fiturUpdate(ftr, dtLama, dtBaru):
+def update_feature(attr, old, new):
+    lokasi_plot = [lokasi_selection.labels[i] for i in lokasi_selection.active]
     
-    plot = [chbx.labels[i] for i in chbx.active]    
-    feature = fitur.value   
-    new_src = newData(plot, feature)
+    feature = feature_select.value
+    
+    new_src = make_dataset(lokasi_plot, feature)
+
     src.data.update(new_src.data)
 
+lokasi_selection = CheckboxGroup(labels=lokasi, active = [0])
+lokasi_selection.on_change('active', update_country)
 
-# In[20]:
+feature_select = Select(options = col_list[2:], value = 'Total Cases', title = 'Feature 1')
+feature_select.on_change('value', update_feature)
 
+#fitur 2
+feature_select2 = Select(options = col_list[2:], value = 'Total Cases', title = 'Feature 2')
+feature_select2.on_change('value', update_feature)
 
+#end fitur 2
 
-chbx = CheckboxGroup(labels=prov, active = [0])
-chbx.on_change('active', provinceUpdate)
+initial_country = [lokasi_selection.labels[i] for i in lokasi_selection.active]
 
+src = make_dataset(initial_country, feature_select.value)
 
-#Drop down 
-fitur = Select(options = DataList[2:], value = 'Daily_Case', title = 'Pilih Fitur')
-fitur.on_change('value', fiturUpdate)
+p = make_plot(src, feature_select.value)
 
-#Default province
-province = [chbx.labels[i] for i in chbx.active]
-
-#Make default ColumnDataSource instance
-src = newData(province, fitur.value)
-
-#Plot  data
-p = plotGraf(src, fitur.value)
-
-# Put controls in a single element
-controls = WidgetBox(fitur, chbx)
+controls = WidgetBox(feature_select, lokasi_selection)
 
 # Create a row layout
 layout = row(controls, p)
 
+#Adding the layout to the current document
+curdoc().add_root(layout)
 
-# In[21]:
 
+#=============================================================
+data2 = pd.read_csv("./data/covid_19_indonesia_time_series_all.csv")
+df2 = data[['continent','location','date','total_cases','new_cases','total_deaths','new_deaths']]
+df2['total_cases'].fillna(0,inplace=True)
+df2['new_cases'].fillna(0,inplace=True)
+df2['total_deaths'].fillna(0,inplace=True)
+df2['new_deaths'].fillna(0,inplace=True)
 
-def tab_barplot(data):
+df_asia = df[df['continent'] == 'Asia']
+df_asia['location'].unique()
+df_asia.drop(columns='continent',inplace=True)
+df_indo = df_asia[df_asia['location'] == 'Indonesia']
+
+df_select = df_asia[df_asia['location'] == 'Indonesia']
+df_select2 = df_asia[df_asia['location'] == 'Malaysia']
+
+#Membuat source data untuk plotting data
+source1 = ColumnDataSource(data={
+    'x' : df_select['date'],
+    'y' : df_select['new_cases'],
     
-    source = ColumnDataSource(data)
-    p = figure(y_range=data['Province'], 
-               title="Jumlah Kasus Tiap Provinsi",
-               plot_height=800,
-               plot_width=800,
-               toolbar_location=None)
+})
+source2 = ColumnDataSource(data={
+    'x' : df_select2['date'],
+    'y' : df_select2['new_cases'],
+})
 
-    p.hbar(y='Province', right='Cumulative_Case', source=source, height=1)
 
-    p.x_range.start = 0
-    p.xaxis.formatter = NumeralTickFormatter(format="0")
+#Mengatur Figure gambar
+fig_gambar = figure(title='Data Covid-19 di Asia',
+                   plot_height=550, plot_width=1000,
+                   x_axis_type = 'datetime',
+                   x_axis_label='Date', y_axis_label='Jumlah Orang',
+                   tools=['pan', 'wheel_zoom', 'save', 'reset'])
+
+#Negara 1
+fig_gambar.line(x='x', y='y',
+              color='coral',
+              line_width=2,
+              source=source1,
+              legend_label ='Negara 1')
+
+#Negara 2
+fig_gambar.line(x='x', y='y',
+              color='cornflowerblue',
+              line_width=2,
+              source=source2,
+              legend_label = 'Negara 2')
+
+#Lokasi & Fitur Legend
+fig_gambar.legend.location = 'top_left'
+fig_gambar.legend.click_policy = 'mute'
+
+#Menambahkan Hover
+fig_gambar.add_tools(HoverTool(tooltips=[
+                                ('Date','@x{%F}'),
+                                ('new_cases', '@y')
+                               ],formatters={'@x': 'datetime'},
+                               mode='vline'))
+
+def update_plot(attr, old, new):
+    #Mengambil pilihan data Negara
+    pilihan1 = select1.value
+    pilihan3 = select3.value
     
-    return Panel(child=p, title="BAR PLOT")
-
-
-# In[22]:
-
-
-df_province = data.groupby(['Province']).sum()
-df_province = df_province.reset_index()
-
-
-# In[23]:
-
-
-df_daily_total = data.groupby(['Date']).sum()
-df_daily_total = df_daily_total.reset_index()
-
-
-# In[24]:
-
-
-def tab_lineplot(data):
+    #Mengambil pilihan Negara
+    pilihan0 = select0.value
+    pilihan2 = select2.value
     
-    source = ColumnDataSource(data)
+    #Membuat data berdasarkan pilihan negara
+    df_select = df_asia[df_asia['location'] == pilihan0]
+    df_select2 = df_asia[df_asia['location'] == pilihan2]
     
-    hover = HoverTool(
-        tooltips=[('Timestamp', '@x{%Y-%m-%d}'), ('Value', '@y')],
-        formatters={'x': 'datetime'},)
-
-    date_range_slider = DateRangeSlider(title="Rentang Tanggal", 
-                                        start=data['Date'].min(), 
-                                        end=data['Date'].max(),
-                                        value=(data['Date'].min(), data['Date'].max()), 
-                                        step=1, 
-                                        width=300)
-
-    p = figure(x_range=df_daily_total['Date'],
-               x_axis_type='datetime',
-               x_axis_label='Date',
-               y_axis_label='Jumlah Kasus',
-               plot_height=500,
-               plot_width=750,
-               title='Jumlah Kasus COVID-19')
+    #Membuat data baru berdasarkan pilihan
+    new_source1 = {
+        'x' : df_select['date'],
+        'y' : df_select[pilihan1]
+    }
+    new_source2 = {
+        'x' : df_select2['date'],
+        'y' : df_select2[pilihan3]
+    }
     
-    p.line('Date', 'Cumulative_Case', source=source)
-    p.add_tools(hover)
-    p.xaxis.formatter = DatetimeTickFormatter(days = ['%F']) 
+    #Memasukan data baru ke source plot
+    source1.data = new_source1
+    source2.data = new_source2
+
+def updateNegara_plot(attr, old, new):
+    #Mengambil pilihan data Negara
+    pilihan0 = select0.value
+    pilihan2 = select2.value
+
+    #Membuat data berdasarkan pilihan negara
+    df_select = df_asia[df_asia['location'] == pilihan0]
+    df_select2 = df_asia[df_asia['location'] == pilihan2]
     
-    callback = CustomJS(args=dict(source=source, ref_source=source), code="""   
-        console.log(cb_obj.value); 
-        const date_from = Date.parse(new Date(cb_obj.value[0]).toDateString());
-        const date_to = Date.parse(new Date(cb_obj.value[1]).toDateString());
-
-        const data = source.data;
-        const ref = ref_source.data;
-
-        const from_pos = ref["x"].indexOf(date_from);
-        const to_pos = ref["x"].indexOf(date_to);
-
-        data["y"] = ref["y"].slice(from_pos, to_pos);
-        data["x"] = ref["x"].slice(from_pos, to_pos);
-
-        source.change.emit();
-    """)
+    #Membuat data baru berdasarkan pilihan
+    new_source1 = {
+        'x' : df_select['date'],
+        'y' : df_select['new_cases']
+    }
+    new_source2 = {
+        'x' : df_select2['date'],
+        'y' : df_select2['new_cases']
+    }
     
-    date_range_slider.js_on_change('value', callback)
-    
-    layout = column(date_range_slider, p)
-    
-    lineplot = Panel(child=layout, title="LINE PLOT")
-    
-    return lineplot
+    #Memasukan data baru ke source plot
+    source1.data = new_source1
+    source2.data = new_source2
 
 
-# In[25]:
+#Pilihan pada Select berupa List Seluruh Negara
+#Membuat pilihan Negara
+option0 = df_asia['location'].unique().tolist()
+
+#Membuat pilihan data Negara
+option1 = df_indo.columns.to_list()
+del option1[0]
+del option1[0]
+
+##Menu pilihan Negara 1
+#Select0 untuk memilih Negara 1
+select0 = Select(
+    options = option0,
+    title = 'Pilih Negara 1',
+    value = 'Indonesia'
+)
+
+#Select1 untuk memilih data Negara 1
+select1 = Select(
+    options = option1,
+    title = 'Pilih Data Negara 1',
+    value = 'new_cases'
+)
+
+##Menu pilihan Negara 2
+#Select2 untuk memilih Negara 2
+select2 = Select(
+    options = option0,
+    title = 'Pilih Negara 2',
+    value = 'Malaysia'
+)
+
+#Select3 untuk memilih data Negara 2
+select3 = Select(
+    options = option1,
+    title = 'Pilih Data Negara 2',
+    value = 'new_cases'
+) 
+
+#Jika select dipilih
+select0.on_change('value', updateNegara_plot)
+select1.on_change('value', update_plot)
+select2.on_change('value', updateNegara_plot)
+select3.on_change('value', update_plot)
 
 
-bar = tab_barplot(df_province)
-line = tab_lineplot(df_daily_total)
-first_panel = Panel(child=layout, title='Death by Cumulative Case ')
-tabs = Tabs(tabs=[line,first_panel,bar])
+#Membuat layout gambar
+layout = row(widgetbox(select0,select1,select2,select3), fig_gambar)
 
+#Membuat Panel gambar
+panel1 = Panel(child=layout, title='Visualisasi Perbandingan Data Covid-19')
 
-# In[26]:
-
-
-show(tabs)
-
-
-# In[27]:
-
+#Membuat Tabel dengan Panel yang ada
+tabs = Tabs(tabs=[panel1,])
 
 curdoc().add_root(tabs)
 
 
-# In[ ]:
-
-
-
-
+#====================================================================
